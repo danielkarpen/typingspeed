ed6ac96b3fef9af175d2535391f5882fb370fcf3
Daniel <danielkarpen97@gmail.com>
2021-03-03 16:30:39 -0600

    simplify component structure add Info component

--- src/components/Main/TimerForm/index.js
+++ src/components/Main/TimerForm/index.js
@@ -1,7 +1,15 @@
-import Form from "./Form";
-
-function TimerForm() {
-  return <Form />;
+function Form() {
+  return (
+    <form className="flex flex-col gap-3 items-center">
+      <label htmlFor="secs" className="text-2xl">
+        How Long Should the Test Run?
+      </label>
+      <input type="number" id="secs" placeholder="secs" className="w-24" />
+      <button className="bg-neon-green cursor-pointer h-16 p-2 rounded-full text-2xl  text-gray-700 w-16 hover:animate-pulse focus:animate-pulse">
+        Go!
+      </button>
+    </form>
+  );
 }
 
-export default TimerForm;
+export default Form;
---
+++ src/lib/index.js
@@ -0,0 +1,1 @@
+export const calcWPM = (str, secs = 60) => Math.round((str.length / 5) * (60 / secs));
