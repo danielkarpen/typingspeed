b3c978b0c841ec87e38507ee65a2d7c768799e91
Daniel <danielkarpen97@gmail.com>
2021-03-04 17:16:03 -0600

    use toKebabCase for for and id attributes

--- src/components/Main/Form/index.js
+++ src/components/Main/Form/index.js
@@ -1,12 +1,20 @@
+import { toKebabCase } from "lib";
 import PropTypes from "prop-types";
 
 const Form = ({ handler, type, label, placeholder, buttonTxt }) => {
+  const kebab = toKebabCase(label);
+
   return (
     <form className="flex flex-col gap-3 items-center" onSubmit={handler}>
-      <label htmlFor="secs" className="text-2xl">
+      <label htmlFor={kebab} className="text-2xl">
         {label}
       </label>
-      <input type={type} id="secs" placeholder={placeholder} className="w-24" />
+      <input
+        type={type}
+        id={kebab}
+        placeholder={placeholder}
+        className="w-24"
+      />
       <button className="bg-neon-green cursor-pointer h-16 p-2 rounded-full text-2xl  text-gray-700 w-16 hover:animate-pulse focus:animate-pulse">
         {buttonTxt}
       </button>
